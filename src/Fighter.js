8057a49211f87dc71d1097c9bcb6013fd12b513a
FREDERIC TICON <fred.ticon@gmail.com>
2023-10-26 13:38:51 +0200

    Update Fighter.js

--- src/Fighter.js
+++ src/Fighter.js
@@ -1,19 +1,33 @@
 const MAX_LIFE = 100;
 
 class Fighter {
-    constructor(name, strength, dexterity) {
+    constructor(name, strength, dexterity,weapon,shield) {
         this.name = name;
         this.strength = strength;
         this.dexterity = dexterity;
         this.life = MAX_LIFE;
+        this.weapon = null;
+        this.shield = null;
     }
+getDamage(){
+
+    this.weapon ? this.strength + this.weapon.damage : this.strength;
+    
+}
+
+getDefense() {
+    this.shield ? this.dexterity + this.shield.preotection : this.dexterity;
+}
+
+
+
 
 
     // Launch a fight
     fight(defender) {
-        const attackPoints = this.getRandomInt(this.strength);
+        const attackPoints = this.getRandomInt(this.getDamage());
 
-        const damages = Math.max(attackPoints - defender.dexterity, 0);
+        const damages = Math.max(attackPoints - defender.getDefense(), 0);
 
         defender.life = Math.max(defender.life - damages, 0);
     }
